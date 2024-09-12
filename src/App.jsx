dcf2a473d6631a1d2763c9bcbd8c41d54e30b6e8
Khvede93 <gio.khvede93@gmail.com>
2024-09-12 17:34:19 +0200

    prepare project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.css';
+import { ScrollIndicator } from './components/scroll-indicator/ScrollIndicator';
+
+function App() {
+  return (
+    <div className='app'>
+      <ScrollIndicator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/scroll-indicator/ScrollIndicator.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './scrollIndicator.css';
+
+export const ScrollIndicator = () => {
+  return <div className='container'>Scroll Indicator</div>;
+};
