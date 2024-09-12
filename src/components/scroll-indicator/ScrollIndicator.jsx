982e71e3c637b7a7423816ce7ff12336123e8769
Khvede93 <gio.khvede93@gmail.com>
2024-09-12 18:19:57 +0200

    prepare component to fetch some random data

--- src/components/scroll-indicator/ScrollIndicator.jsx
+++ src/components/scroll-indicator/ScrollIndicator.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './scrollIndicator.css';
 
-export const ScrollIndicator = () => {
+export const ScrollIndicator = ({ url }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [errorM, setErrorM] = useState('');
+
+  async function fetchData(url) {
+    try {
+      setLoading(true);
+    } catch (e) {
+      setErrorM(e.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(url);
+  }, [url]);
+
   return <div className='container'>Scroll Indicator</div>;
 };
